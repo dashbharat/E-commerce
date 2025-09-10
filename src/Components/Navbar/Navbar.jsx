2a5a818cd41e2937c294501c34ece906cd3c57d3
Bharat Bhusan Dash <bharatdash2001@gmail.com>
2025-09-11 00:21:58 +0530

    I completed Navigation and create file in pagesection and set routes

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import './Navbar.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
+import { Link } from "react-router-dom";
+
 
 export const Navbar = () => {
   //!for changing the effect of option in hr line
@@ -15,14 +17,14 @@ export const Navbar = () => {
         <p>SHOPZY</p>
       </div>
       <ul className='nav-menu'>
-        <li onClick={()=>{setMenu("shop")}}>Shop{menu==="shop"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("mens")}}>Men {menu==="mens"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("womens")}}>Women {menu==="womens"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("kids")}}>Kids {menu==="kids"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("shop")}}> <Link to='/' style={{textDecoration:"none"}}>Shop</Link> {menu==="shop"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("mens")}}> <Link to='/mens' style={{textDecoration:"none"}}>Men</Link> {menu==="mens"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("womens")}}> <Link to='/womens' style={{textDecoration:"none"}}>Women</Link> {menu==="womens"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("kids")}}> <Link to='/kids' style={{textDecoration:"none"}}>Kids</Link> {menu==="kids"?<hr/>:<></>}</li>
       </ul>
       <div className="nav-login-cart">
-        <button>Login</button>
-        <img src={cart_icon} alt="Cart Icon" />
+       <Link to='/login' style={{textDecoration:"none"}}> <button>Login</button> </Link>
+       <Link to='/cart' style={{textDecoration:"none"}}> <img className="cart-icon-size" src={cart_icon} alt="Cart Icon" /> </Link>
         <div className="nav-cart-count">0 </div>
       </div>
 
