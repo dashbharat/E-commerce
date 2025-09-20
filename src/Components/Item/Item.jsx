7c18d489e2ba45b13d2a56eadcd42200c22d002c
Bharat Bhusan Dash <bharatdash2001@gmail.com>
2025-09-20 21:41:31 +0530

    i have completed the Related Product section

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 const Item = (props) => {
   return (
     <div className='item'>
-     <Link to={`/product/${props.id}`}> <img className='item_images' src={props.image} alt="" /></Link>
+     <Link to={`/product/${props.id}`}> <img onClick={window.scrollTo(0,0)} className='item_images' src={props.image} alt="" /></Link>
       <p>{props.name}</p>
       <div className='item-prices'>
         <div className="item-price-new">
@@ -16,9 +16,6 @@ const Item = (props) => {
           ${props.old_price}
         </div>
       </div>
-        
-
-
     </div>
   )
 }
